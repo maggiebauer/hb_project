b967ae6b1605da5ad1b473b5737a8870fb9cd669
Maggie Bauer <margaretbauer404@gmail.com>
2018-09-16 00:33:03 -0700

    working on seed.py - csv opening and feeding into first CBCompany objects

--- model.py
+++ model.py
@@ -12,144 +12,144 @@ db = SQLAlchemy()
 ################################################################################
 # Full Contact Data
 
-class FCCompany(db.Model):
-    """ Full Contact company info  """
+# class FCCompany(db.Model):
+#     """ Full Contact company info  """
 
-    __tablename__ = 'fc_companies'
+#     __tablename__ = 'fc_companies'
 
-    fc_company_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    fc_company_name = db.Column(db.String(100), nullable=False)
-    fc_company_domain = db.Column(db.String(100), nullable=False)
-    logo_image_id = db.Column(db.Integer, db.ForeignKey('images.image_id'))
-    location = db.Column(db.String(300))
-    founded = db.Column(db.String(25))
-    num_employees = db.Column(db.String(25))
-    bio = db.Column(db.String(500))
+#     fc_company_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+#     fc_company_name = db.Column(db.String(100), nullable=False)
+#     fc_company_domain = db.Column(db.String(100), nullable=False) 
+#     logo_image_id = db.Column(db.Integer, db.ForeignKey('images.image_id'))
+#     location = db.Column(db.String(300))
+#     founded = db.Column(db.String(25))
+#     num_employees = db.Column(db.String(25))
+#     bio = db.Column(db.String(500))
 
-    social_media = db.relationship('SocialMediaLink', backref=db.backref('fc_company'))
-    companay_links = db.relationship('CompanyLink', backref=db.backref('fc_company'))
-    people = db.relationship('Person', backref=db.backref('fc_company'))
-    industries = db.relationship('CompanyIndustry', backref=db.backref('fc_companies'))
-    images = db.relationship('Image', backref=db.backref('fc_company'))
-    cb_company = db.relationship('CBCompany', backref=db.backref('fc_company'))
+#     social_media = db.relationship('SocialMediaLink', backref=db.backref('fc_company'))
+#     companay_links = db.relationship('CompanyLink', backref=db.backref('fc_company'))
+#     people = db.relationship('Person', backref=db.backref('fc_company'))
+#     industries = db.relationship('CompanyIndustry', backref=db.backref('fc_companies'))
+#     images = db.relationship('Image', backref=db.backref('fc_company'))
+#     cb_company = db.relationship('CBCompany', backref=db.backref('fc_company'))
 
-    def __repr__(self):
-        """Provide helpful representation when printed."""
+#     def __repr__(self):
+#         """Provide helpful representation when printed."""
 
-        repr_str = '<FCCompany: id:{}, name:{}, location:{}>'
-        return repr_str.format(self.fc_company_id, self.fc_company_name, self.location)
-
-
-class SocialMediaLink(db.Model):
-    """ Full Contact company social media sites  """
-
-    __tablename__ = 'social_media'
-
-    sm_link_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    fc_company_id = db.Column(db.Integer, db.ForeignKey('fc_companies.fc_company_id'), nullable=False)
-    sm_type_id = db.Column(db.String(10), db.ForeignKey('social_media_sites.sm_type_id'), nullable=False)
-    sm_site_url = db.Column(db.String(200), nullable=False)
-
-    sm_type = db.relationship('SocialMediaType', backref=db.backref('social_media_links'))
-    image = db.relationship('Image')
-
-    def __repr__(self):
-        """Provide helpful representation when printed."""
-        repr_str = '<SocialMediaLink: sm_type_id:{}, fc_company_id:{}, sm_site_url:{}>'
-        return repr_str.format(self.sm_type_id, self.fc_company_id, self.sm_site_url)
+#         repr_str = '<FCCompany: id:{}, name:{}, location:{}>'
+#         return repr_str.format(self.fc_company_id, self.fc_company_name, self.location)
 
 
-class SocialMediaType(db.Model):
-    """ Full Contact social media types  """
+# class SocialMediaLink(db.Model):
+#     """ Full Contact company social media sites  """
 
-    __tablename__ = 'sm_types'
+#     __tablename__ = 'social_media'
 
-    sm_type_id = db.Column(db.String(10), primary_key=True)
-    sm_name = db.Column(db.String(100), nullable=False)
-    sm_logo_image_id = db.Column(db.Integer, nullable=False)
+#     sm_link_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+#     fc_company_id = db.Column(db.Integer, db.ForeignKey('fc_companies.fc_company_id'), nullable=False)
+#     sm_type_id = db.Column(db.String(10), db.ForeignKey('social_media_sites.sm_type_id'), nullable=False)
+#     sm_site_url = db.Column(db.String(200), nullable=False)
 
-    def __repr__(self):
-        """Provide helpful representation when printed."""
-        repr_str = '<SocialMediaType: sm_type_id:{}, sm_name:{}, sm_logo_image_id:{}>'
-        return repr_str.format(self.sm_type_id, self.sm_name, self.sm_logo_image_id)
+#     sm_type = db.relationship('SocialMediaType', backref=db.backref('social_media_links'))
+#     image = db.relationship('Image')
 
-
-class CompanyLink(db.Model):
-    """ Full Contact company links  """
-
-    __tablename__ = 'company_links'
-
-    company_link_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    fc_company_id = db.Column(db.Integer, db.ForeignKey('fc_companies.fc_company_id'), nullable=False)
-    link_type = db.Column(db.String(50), nullable=False)
-    link_url = db.Column(db.String(300), nullable=False)
-
-    def __repr__(self):
-        """Provide helpful representation when printed."""
-        repr_str = '<CompanyLink: link_type:{}, fc_company_id:{}>'
-        return repr_str.format(self.link_type, self.fc_company_id)
+#     def __repr__(self):
+#         """Provide helpful representation when printed."""
+#         repr_str = '<SocialMediaLink: sm_type_id:{}, fc_company_id:{}, sm_site_url:{}>'
+#         return repr_str.format(self.sm_type_id, self.fc_company_id, self.sm_site_url)
 
 
-class Person(db.Model):
-    """ Full Contact people """
+# class SocialMediaType(db.Model):
+#     """ Full Contact social media types  """
 
-    __tablename__ = 'people'
+#     __tablename__ = 'sm_types'
 
-    person_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    fc_company_id = db.Column(db.Integer, db.ForeignKey('fc_companies.fc_company_id'), nullable=False)
-    title = db.Column(db.String(50))
-    person_image_id = db.Column(db.Integer, db.ForeignKey('fc_companies.fc_company_id'))
+#     sm_type_id = db.Column(db.String(10), primary_key=True)
+#     sm_name = db.Column(db.String(100), nullable=False)
+#     sm_logo_image_id = db.Column(db.Integer, nullable=False)
 
-    def __repr__(self):
-        """Provide helpful representation when printed."""
-        repr_str = '<People: person_id{}, fc_company_id:{}, title:{}>'
-        return repr_str.format(self.person_id, self.fc_company_id, self.title)
-
-
-class IndustryType(db.Model):
-    """ Full Contact industry types  """
-
-    __tablename__ = 'industry_types'
-
-    industry_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    industry_name = db.Column(db.String(50), nullable=False)
-
-    fc_companies = db.relationship('CompanyIndustry', backref=db.backref('industry_name'))
-
-    def __repr__(self):
-        """Provide helpful representation when printed."""
-        repr_str = '<IndustryType: industry_id:{}, industry_name:{}>'
-        return repr_str.format(self.industry_id, self.industry_name)
+#     def __repr__(self):
+#         """Provide helpful representation when printed."""
+#         repr_str = '<SocialMediaType: sm_type_id:{}, sm_name:{}, sm_logo_image_id:{}>'
+#         return repr_str.format(self.sm_type_id, self.sm_name, self.sm_logo_image_id)
 
 
-class CompanyIndustry(db.Model):
-    """ Full Contact companies with industries  """
+# class CompanyLink(db.Model):
+#     """ Full Contact company links  """
 
-    __tablename__ = 'company_industries'
+#     __tablename__ = 'company_links'
 
-    company_with_industry_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    industry_id = db.Column(db.Integer, db.ForeignKey('industry_types.industry_id'), nullable=False)
-    fc_company_id = db.Column(db.Integer, db.ForeignKey('fc_companies.fc_company_id'), nullable=False)
+#     company_link_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+#     fc_company_id = db.Column(db.Integer, db.ForeignKey('fc_companies.fc_company_id'), nullable=False)
+#     link_type = db.Column(db.String(50), nullable=False)
+#     link_url = db.Column(db.String(300), nullable=False)
 
-    def __repr__(self):
-        """Provide helpful representation when printed."""
-        repr_str = '<CompanyIndustry: industry_id:{}, fc_company_id:{}>'
-        return repr_str.format(self.industry_id, self.fc_company_id)
+#     def __repr__(self):
+#         """Provide helpful representation when printed."""
+#         repr_str = '<CompanyLink: link_type:{}, fc_company_id:{}>'
+#         return repr_str.format(self.link_type, self.fc_company_id)
 
 
-class Image(db.Model):
-    """ Full Contact images  """
+# class Person(db.Model):
+#     """ Full Contact people """
 
-    __tablename__ = 'images'
+#     __tablename__ = 'people'
 
-    imgage_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    image_type = db.Column(db.String(50), nullable=False)
-    image_url = db.Column(db.String(300), nullable=False)
+#     person_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+#     fc_company_id = db.Column(db.Integer, db.ForeignKey('fc_companies.fc_company_id'), nullable=False)
+#     title = db.Column(db.String(50))
+#     person_image_id = db.Column(db.Integer, db.ForeignKey('images.image_id'))
 
-    def __repr__(self):
-        """Provide helpful representation when printed."""
-        repr_str = '<Image: image_id:{}, image_type:{}>'
-        return repr_str.format(self.image_id, self.image_type)
+#     def __repr__(self):
+#         """Provide helpful representation when printed."""
+#         repr_str = '<People: person_id{}, fc_company_id:{}, title:{}>'
+#         return repr_str.format(self.person_id, self.fc_company_id, self.title)
+
+
+# class IndustryType(db.Model):
+#     """ Full Contact industry types  """
+
+#     __tablename__ = 'industry_types'
+
+#     industry_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+#     industry_name = db.Column(db.String(50), nullable=False)
+
+#     fc_companies = db.relationship('CompanyIndustry', backref=db.backref('industry_name'))
+
+#     def __repr__(self):
+#         """Provide helpful representation when printed."""
+#         repr_str = '<IndustryType: industry_id:{}, industry_name:{}>'
+#         return repr_str.format(self.industry_id, self.industry_name)
+
+
+# class CompanyIndustry(db.Model):
+#     """ Full Contact companies with industries  """
+
+#     __tablename__ = 'company_industries'
+
+#     company_with_industry_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+#     industry_id = db.Column(db.Integer, db.ForeignKey('industry_types.industry_id'), nullable=False)
+#     fc_company_id = db.Column(db.Integer, db.ForeignKey('fc_companies.fc_company_id'), nullable=False)
+
+#     def __repr__(self):
+#         """Provide helpful representation when printed."""
+#         repr_str = '<CompanyIndustry: industry_id:{}, fc_company_id:{}>'
+#         return repr_str.format(self.industry_id, self.fc_company_id)
+
+
+# class Image(db.Model):
+#     """ Full Contact images  """
+
+#     __tablename__ = 'images'
+
+#     image_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+#     image_type = db.Column(db.String(50), nullable=False)
+#     image_url = db.Column(db.String(300), nullable=False)
+
+#     def __repr__(self):
+#         """Provide helpful representation when printed."""
+#         repr_str = '<Image: image_id:{}, image_type:{}>'
+#         return repr_str.format(self.image_id, self.image_type)
 
 
 ################################################################################
@@ -163,16 +163,16 @@ class CBCompany(db.Model):
 
     cb_company_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     cb_company_name = db.Column(db.String(100), nullable=False)
-    cb_company_domain = db.Column(db.String(100), nullable=False)
+    # cb_company_domain = db.Column(db.String(100), nullable=False)
     cb_permalink = db.Column(db.String(300), nullable=False)
     cb_url = db.Column(db.String(300))
-    market_type_id = db.Column(db.Integer, db.ForeignKey('market_types.market_type_id'))
-    state_code_id = db.Column(db.Integer, db.ForeignKey('state_codes.state_code_id'))
-    fc_company_id = db.Column(db.Integer, db.ForeignKey('fc_companies.fc_company_id'))
+    # market_type_id = db.Column(db.Integer, db.ForeignKey('market_types.market_type_id'))
+    # state_code_id = db.Column(db.Integer, db.ForeignKey('state_codes.state_code_id'))
+    # fc_company_id = db.Column(db.Integer, db.ForeignKey('fc_companies.fc_company_id'))
 
-    funding_rounds = db.relationship('FundingRound', backref=db.backref('cb_company'))
-    hq_state_code = db.relationship('StateCode', backref=db.backref('cb_companies'))
-    market_type = db.relationship('MarketType', backref=db.backref('cb_companies'))
+    # funding_rounds = db.relationship('FundingRound', backref=db.backref('cb_company'))
+    # hq_state_code = db.relationship('StateCode', backref=db.backref('cb_companies'))
+    # market_type = db.relationship('MarketType', backref=db.backref('cb_companies'))
 
     def __repr__(self):
         """Provide helpful representation when printed."""
@@ -248,21 +248,21 @@ class MarketType(db.Model):
 
 # Helper functions
 
-# def connect_to_db(app):
-#     """Connect the database to our Flask app."""
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
 
-#     # Configure to use our PstgreSQL database
-#     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///company_insights'
-#     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#     db.app = app
-#     db.init_app(app)
+    # Configure to use our PstgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///company_insights'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
 
 
-# if __name__ == "__main__":
-#     # As a convenience, if we run this module interactively, it will leave
-#     # you in a state of being able to work with the database directly.
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
 
-#     from server import app
-#     connect_to_db(app)
-#     print("Connected to DB.")
+    from server import app
+    connect_to_db(app)
+    print("Connected to DB.")
 
--- seed.py
+++ seed.py
@@ -1,20 +1,47 @@
-"""Utility file to seed ratings database from MovieLens data in seed_data/"""
+"""Utility file to seed company_insights database from FullContact and Crunchbase data in seed_data/"""
 
 from sqlalchemy import func
-from model import FCCompany
-from model import SocialMediaLink
-from model import SocialMediaType
-from model import CompanyLink
-from model import Person
-from model import IndustryType
-from model import CompanyIndustry
-from model import Image
+# from model import FCCompany
+# from model import SocialMediaLink
+# from model import SocialMediaType
+# from model import CompanyLink
+# from model import Person
+# from model import IndustryType
+# from model import CompanyIndustry
+# from model import Image
 from model import CBCompany
-from model import FundingRound
-from model import FundingType
-from model import StateCode
-from model import MarketType
+# from model import FundingRound
+# from model import FundingType
+# from model import StateCode
+# from model import MarketType
 
 from model import connect_to_db, db
-from server import app
+# from server import app
 import datetime
+import csv
+
+
+def load_cb_companies():
+    """Load cbcompanies from seed_data/cbcompanies into database."""
+
+    print("CBCompanies")
+
+    # # Delete all rows in table, so if we need to run this a second time,
+    # # we won't be trying to add duplicate users
+    # User.query.delete()
+
+    # Read cbcompanies.csv file and insert data
+    company_csv = open('test_companies.csv')
+    company_reader = csv.reader(company_csv)
+    
+    for row in company_reader:        
+        cb_company = CBCompany(cb_company_name=row[1], 
+                    cb_url=row[2],
+                    cb_permalink=row[0])
+
+    # We need to add to the session or it won't ever be stored
+    #     db.session.add(cb_company)
+
+
+    # # Once we're done, we should commit our work
+    # db.session.commit()
